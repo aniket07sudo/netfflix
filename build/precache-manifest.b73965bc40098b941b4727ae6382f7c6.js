4766a783b7d922be26a69b4d11063e397e08b86a
unknown <Aniket@ANIKET.Aniket>
2020-12-02 01:14:27 +0530

    first

---
+++ build/precache-manifest.b73965bc40098b941b4727ae6382f7c6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a2cb5033510339ea647cd24f63a03d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b3750a8cc6789d16c0",
+    "url": "/static/css/main.72d29ec4.chunk.css"
+  },
+  {
+    "revision": "2e64a236d7de47e68904",
+    "url": "/static/js/2.3dc58261.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3dc58261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b3750a8cc6789d16c0",
+    "url": "/static/js/main.dfe72211.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "73e25d1dae3deac964df43111c04c973",
+    "url": "/static/media/devices-video.73e25d1d.m4v"
+  },
+  {
+    "revision": "186a706493dd515e30f8ad682d068578",
+    "url": "/static/media/devices.186a7064.png"
+  },
+  {
+    "revision": "dd3cb78db78c3e0a13c9b77ee19deee6",
+    "url": "/static/media/downloading.dd3cb78d.gif"
+  },
+  {
+    "revision": "7cd1fe48c5f48c3a9d31fcb472f4c8a3",
+    "url": "/static/media/front.7cd1fe48.jpg"
+  },
+  {
+    "revision": "8f1153c78a37a7915e06220b40b42ab4",
+    "url": "/static/media/logoo.8f1153c7.svg"
+  },
+  {
+    "revision": "33cdca2a652d79948f0b0ea2bdd6d235",
+    "url": "/static/media/next.33cdca2a.svg"
+  },
+  {
+    "revision": "a48333d43c19612ed61987fa5dbff3c5",
+    "url": "/static/media/phone.a48333d4.jpg"
+  },
+  {
+    "revision": "32614af2bcf33f311cf41f45f1188c00",
+    "url": "/static/media/plus.32614af2.svg"
+  },
+  {
+    "revision": "587e040f20259792094901ca9739669e",
+    "url": "/static/media/stranger.587e040f.png"
+  },
+  {
+    "revision": "a585f6f325641f820e3272f3ec0086ed",
+    "url": "/static/media/tv-video.a585f6f3.m4v"
+  },
+  {
+    "revision": "77994a67327ba957dfd880e33a91f041",
+    "url": "/static/media/tv.77994a67.png"
+  }
+]);
